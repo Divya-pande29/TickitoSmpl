9afb65c286b2af39b6b102a44c9bbe34544b5c03
Divya-pande29 <divyapande119@gmail.com>
2026-07-27 09:34:30 +0530

    updated userserviceimpl, userservice

--- Tikito/src/main/java/com/sunbeam/tikito/controllers/UserController.java
+++ Tikito/src/main/java/com/sunbeam/tikito/controllers/UserController.java
@@ -1,69 +1,84 @@
 package com.sunbeam.tikito.controllers;
 		
-		import org.springframework.web.bind.annotation.DeleteMapping;
+		import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 		import org.springframework.web.bind.annotation.GetMapping;
 		import org.springframework.web.bind.annotation.PathVariable;
 		import org.springframework.web.bind.annotation.PostMapping;
 		import org.springframework.web.bind.annotation.PutMapping;
 		import org.springframework.web.bind.annotation.RequestBody;
 		import org.springframework.web.bind.annotation.RequestMapping;
-		import org.springframework.web.bind.annotation.RestController;
-		import com.sunbeam.tikito.dto.UserDto;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.sunbeam.tikito.dto.UserDto;
+import com.sunbeam.tikito.entity.UserEntity;
 import com.sunbeam.tikito.serviceimpl.UserServiceImpl;
+import com.sunbeam.tikito.services.UserService;
 import com.sunbeam.tikito.utils.Resp;
 
-		@RequestMapping("/users")
-		@RestController
-		public class UserController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 
-		    private UserServiceImpl userService;
 
-		    public UserController(UserServiceImpl userService) {
-		        this.userService = userService;
-		    }
+@RestController
+@RequestMapping("/tikito/user")
+public class UserController {
 
-		    // Registration
-		    @PostMapping
-		    public Resp<?> registerUser( @RequestBody UserDto dto) {
-		    	dto = userService.register(dto);
-		        return Resp.success(dto);
-		    }
+    private final UserService userService;
 
-//		    // Login
-//		    @PostMapping("/login")
-//		    public Resp<?> loginUser(@RequestBody UserDto dto) {
-//		        dto = userService.login(dto);
-//		        return Resp.success(dto);
-//		    }
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
-		    // Get Profile
-		    @GetMapping("/{userId}")
-		    public Resp<?> getProfile(@PathVariable Long userId) {
-		        UserDto dto = userService.getProfile(userId);
-		        return Resp.success(dto);
-		    }
+    // Public
+    @PostMapping("/register")
+    public Resp<?> register(@RequestBody UserDto dto) {
 
-		    // Update Password
-		    @PutMapping("/update-password")
-		    public Resp<?> updatePassword(@RequestBody UserDto dto) {
-		        String msg = userService.updatePassword(dto);
-		        return Resp.success(msg);
-		    }
+        return Resp.success(userService.register(dto));
+    }
 
-		    // Forgot Password
-		    @PutMapping("/forgot-password")
-		    public Resp<?> forgotPassword(@RequestBody UserDto dto) {
-		        String msg = userService.forgotPassword(dto);
-		        return Resp.success(msg);
-		    }
+    // Logged-in user
+    @GetMapping("/profile")
+    public Resp<?> getProfile(
+            @AuthenticationPrincipal UserEntity loggedInUser) {
 
-		    // Delete Account
-		    @DeleteMapping("/{userId}")
-		    public Resp<?> deleteAccount(@PathVariable Long userId) {
-		        String msg = userService.deleteAccount(userId);
-		        return Resp.success(msg);
-		    }
+        return Resp.success(
+                userService.getProfile(loggedInUser.getUserId()));
+    }
 
-	   
+    // Logged-in user
+    @PutMapping("/password")
+    public Resp<?> updatePassword(
+            @RequestBody UserDto dto,
+            @AuthenticationPrincipal UserEntity loggedInUser) {
 
+        return Resp.success(
+                userService.updatePassword(
+                        loggedInUser.getUserId(),
+                        dto));
+    }
+
+    // Public
+    @PutMapping("/forgot-password")
+    public Resp<?> forgotPassword(
+            @RequestBody UserDto dto) {
+
+        return Resp.success(
+                userService.forgotPassword(dto));
+    }
+
+    // Logged-in user
+    @DeleteMapping
+    public Resp<?> deleteAccount(
+            @AuthenticationPrincipal UserEntity loggedInUser) {
+
+        return Resp.success(
+                userService.deleteAccount(
+                        loggedInUser.getUserId()));
+    }
 }
+
