032b7c7e74c5437d7184f91b67e3c34e23c2fe64
Divya-pande29 <divyapande119@gmail.com>
2026-07-27 08:26:53 +0530

    added custome user deatil

--- Tikito/src/main/java/com/sunbeam/tikito/serviceimpl/CustomUserDetailsService.java
+++ Tikito/src/main/java/com/sunbeam/tikito/serviceimpl/CustomUserDetailsService.java
@@ -1,5 +1,22 @@
 package com.sunbeam.tikito.serviceimpl;
 
-public class CustomUserDetailsService {
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.sunbeam.tikito.daos.UserDao;
+import com.sunbeam.tikito.entity.UserEntity;
+
+public class CustomUserDetailsService implements UserDetailsService{
+
+	private UserDao userdao;
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		UserEntity user = userdao.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("user not found"));
+		if (user == null)
+			throw new UsernameNotFoundException("user not found");
+		return user;
+	}
+	
 
 }
