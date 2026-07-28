4c5562e42c0938262173e986fcd0d764148a6b8d
Divya-pande29 <divyapande119@gmail.com>
2026-07-28 19:49:40 +0530

    Updated Android UI and activity package structure

--- Tikito_mobile_apps/androidApp/app/src/main/java/com/example/tikito/activity/ConcertActivity.java
+++ Tikito_mobile_apps/androidApp/app/src/main/java/com/example/tikito/activity/ConcertActivity.java
@@ -1,4 +1,4 @@
-package com.example.tikito;
+package com.example.tikito.activity;
 
 import android.os.Bundle;
 import android.widget.Button;
@@ -6,11 +6,9 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.tikito.R;
 
 public class ConcertActivity extends AppCompatActivity {
     private ImageView btnBack, btnSearch;
