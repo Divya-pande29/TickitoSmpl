e1c468681991a6490bfd07055ab02659dca8c5e7
Divya-pande29 <divyapande119@gmail.com>
2026-07-29 22:11:06 +0530

    Added Home and Review fragments and updated UI components

--- Tikito_mobile_apps/androidApp/app/src/main/java/com/example/tikito/services/EventAPI.java
+++ Tikito_mobile_apps/androidApp/app/src/main/java/com/example/tikito/services/EventAPI.java
@@ -1,31 +1,30 @@
 package com.example.tikito.services;
 
 
+import com.example.tikito.entities.ApiResponse;
 import com.example.tikito.entities.Event;
 
 
 import java.util.List;
 
+import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 public interface EventAPI {
 
 
-    // Get All Events
     @GET("tikito/events")
-    Call<Resp<List<Event>>> getAllEvents();
+    Call<ApiResponse<List<Event>>> getAllEvents();
 
-    // Get Events by Type
     @GET("tikito/events/type/{eventType}")
-    Call<Resp<List<Event>>> getEventsByType(
+    Call<ApiResponse<List<Event>>> getEventsByType(
             @Path("eventType") String eventType
     );
 
-    // Get Event by Id
     @GET("tikito/events/{eventId}")
-    Call<Resp<Event>> getEventById(
+    Call<ApiResponse<Event>> getEventById(
             @Path("eventId") int eventId
     );
-}
 
+}
