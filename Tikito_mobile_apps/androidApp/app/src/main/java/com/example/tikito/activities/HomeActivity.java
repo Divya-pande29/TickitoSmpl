b663ff5334fbf6a59b512187bddb46ab81bb3797
techAmoghh <amoghhambarde123@gmail.com>
2026-07-30 00:47:34 +0530

    Done with login-registration-login-home flow successfully

--- Tikito_mobile_apps/androidApp/app/src/main/java/com/example/tikito/activities/HomeActivity.java
+++ Tikito_mobile_apps/androidApp/app/src/main/java/com/example/tikito/activities/HomeActivity.java
@@ -11,20 +11,38 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.tikito.R;
+import com.example.tikito.utils.SessionManager;
 
 public class HomeActivity extends AppCompatActivity {
 
-    Button btn;
+    Button btn, btnLogout  ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
         btn = findViewById(R.id.book);
+        btnLogout = findViewById(R.id.btnLogout);
+
         btn.setOnClickListener(v ->
         {
             Intent intent = new Intent(this, BookSeatActivity.class);
             startActivity(intent);
+        });
+
+        btnLogout.setOnClickListener(view -> {
+
+            // Clear saved session
+            SessionManager sessionManager = new SessionManager(HomeActivity.this);
+            sessionManager.logout();
+
+            // Go back to Login screen
+            Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+
+            finish();
+
         });
     }
 }
