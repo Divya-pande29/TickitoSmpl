1aa306b84e987761f5df87ebf0b443e348dd2868
unknown <smgtiwari@gmail.com>
2026-07-30 01:34:38 +0530

    Created a Upload folder in the directory.Created UploadService,UplooadServiceImpl,UploadController and WebConfig classes.Also uploaded a few posters

--- Tikito/src/main/java/com/sunbeam/tikito/security/SecurityConfig.java
+++ Tikito/src/main/java/com/sunbeam/tikito/security/SecurityConfig.java
@@ -63,6 +63,7 @@ public class SecurityConfig
 								   .requestMatchers("/tikito/venue/admin/**").hasRole("ADMIN")
 								   .requestMatchers("/tikito/shows", "/tikito/shows/*", "/tikito/shows/event/*", "/tikito/shows/date/*", "/tikito/shows/time/*").permitAll()
 								   .requestMatchers("/tikito/shows/admin/**").hasRole("ADMIN")
+								   .requestMatchers("/posters/**").permitAll()
 								   .anyRequest().authenticated())
 								   .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
 								   .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
